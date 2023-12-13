d7510538dae863f6589b6cdd3f91f5b8bcc71ab2
heeryoung <winterbooks@naver.com>
2023-12-13 10:42:20 +0900

    Create the Root URL Route

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# Image-to-PDF Converter with HTML, CSS, JS, and NodeJS
+
 [How to Build an Online Image-to-PDF Converter with HTML, CSS, JS, and NodeJS](https://www.freecodecamp.org/news/build-an-online-image-to-pdf-converter-with-html-css-js-nodejs/)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var path = require("path");
+
+// send the file "public/html/index.html" to the client
+router.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "..", "/public/html/index.html"));
 });
 
 module.exports = router;
